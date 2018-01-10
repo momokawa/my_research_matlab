fe645c3b7e7d5ef411f351c9217c79dca9bf4aab
momo <momoko.k.0421@gmail.com>
2018-01-10 14:02:24 +0900

    Add generate_torque_image.m

---
+++ models/generate_torque_image.m
@@ -0,0 +1,28 @@
+% generate each joint's torque figure
+
+% bracket_1_2_joint_torque
+figure;
+plot(bracket_1_2_joint_torque);
+title("bracket_1_2_joint_torque");
+
+% bracket_2_3_joint_torque
+figure;
+plot(bracket_2_3_joint_torque);
+title("bracket_2_3_joint_torque");
+
+% bracket_3_4_joint_torque
+figure;
+plot(bracket_3_4_joint_torque);
+title("bracket_3_4_joint_torque");
+
+% bracket_5_6_joint_torque
+figure;
+plot(bracket_5_6_joint_torque);
+title("bracket_5_6_joint_torque");
+
+% bracket_6_7_joint_torque
+figure;
+plot(bracket_6_7_joint_torque);
+title("bracket_6_7_joint_torque");
+
+% TODO: calculate max i from max torque
