e39c77ca7fcd477f18a71ed442d64e92144dda15
momo <momoko.k.0421@gmail.com>
2018-01-28 23:57:05 +0900

    Add revised trajectory data!

---
+++ models/sample_trajectory_2/generate_joint_mat_file.m
@@ -0,0 +1,9 @@
+% this file generages mat file from txt file
+
+generate_joint_mat_func('sample_trajectory_2_joint_1_2', 11);
+generate_joint_mat_func('sample_trajectory_2_joint_2_3', 12);
+generate_joint_mat_func('sample_trajectory_2_joint_3_4', 13);
+generate_joint_mat_func('sample_trajectory_2_joint_4_5', 14);
+generate_joint_mat_func('sample_trajectory_2_joint_5_6', 15);
+generate_joint_mat_func('sample_trajectory_2_joint_6_7', 16);
+
---
+++ models/sample_trajectory_2/generate_joint_mat_func.m
@@ -0,0 +1,18 @@
+function generate_joint_mat_func(mat_filename, raw_num)
+% this function generate mat_file of a joint with specific raw_num's data
+% mat_filename: bracket_1_2_joint.mat
+% raw_num: the num of raw where each joint array exists
+
+data_input = readtable('sample_trajectory_2.txt', 'Delimiter', 'comma');
+time_array = 0:0.01:0.01*923; % 0.01sec interval array of time ( 100hz)
+joint_position_table = data_input(:, raw_num); % get the joint's position arra
+joint_position_array = table2array(joint_position_table); % convert table to array
+
+% create mat file with required structs
+joint_position_trajectory.time = time_array';
+joint_position_trajectory.signals.values = joint_position_array;
+joint_position_trajectory.signals.dimensions = 1;
+joint_position_trajectory.signals.label = 'joint_position';
+save(mat_filename, 'joint_position_trajectory');
+
+end
