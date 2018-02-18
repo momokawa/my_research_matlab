9caf29d6ee5465cd35e6c4c9d1e0baca374b3d9c
momo <momoko.k.0421@gmail.com>
2018-02-18 15:38:52 +0900

    Add velocity output and change dir to save files from simulation, and regenerate imeges/videos along with it

--- models/create_video_simulation.m
+++ models/create_video_simulation.m
@@ -1,6 +1,6 @@
 % create video from simulation
 
-model_name = 'my_robot_simulation';
+model_name = 'second_simulation';
 sim(model_name);
 
 videoName = 'sample_trajectory_3/video/torque_animation';
